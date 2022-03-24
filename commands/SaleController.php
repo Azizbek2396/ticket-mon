1bf551d7cb4e69789caba239c11a4cab74872256
Azizbek2396 <aziznurmuhammadov1996@gmail.com>
2022-03-24 16:02:34 +0500

    cron for sync with EESBO added

---
+++ commands/SaleController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace app\commands;
+
+use app\models\Events;
+use app\models\Saver;
+use GuzzleHttp\Client;
+use yii\console\Controller;
+
+class SaleController extends Controller
+{
+    public function actionAuth()
+    {
+        $client = new Client();
+        $url = 'https://cabinet.cultureticket.uz/api/CultureTicket/Token';
+
+        $res = $client->request('POST', $url, [
+            'json' => [
+                "login" => 'umar@iticket.uz',
+                "password" => '123456'
+            ],
+            'verify' => false
+        ]);
+
+        $json = json_decode($res->getBody()->getContents(), true);
+        $path = dirname(__DIR__, 1) . '/web/data/auth';
+
+        if (isset($json['result']['accessToken'])){
+            file_put_contents($path, $json['result']['accessToken']);
+        }
+        var_dump($json);
+    }
+
+    public function getToken()
+    {
+        $token = file_get_contents(dirname(__DIR__, 1) . '/web/data/auth');
+        return $token;
+    }
+
+    public function getResponse($url) {
+        $client = new Client();
+        $res = $client->request('GET', $url, [
+            'headers' => [
+                'Authorization' => "Bearer " . $this->getToken(),
+            ],
+            'verify' => false
+        ]);
+
+        return $res;
+    }
+
+    public function actionSold()
+    {
+        $events = Events::find()->all();
+
+        foreach ($events as $event) {
+            $url1 ='https://cabinet.cultureticket.uz/api/CultureTicket/SessionTickets/' . $event->session_id;
+            $res = $this->getResponse($url1);
+            $tickets = json_decode($res->getBody()->getContents(), true);
+
+            $url2 = 'https://cabinet.cultureticket.uz/api/CultureTicket/PalaceHallSeats/' . $event->hall;
+            $res2 = $this->getResponse($url2);
+            $seats = json_decode($res2->getBody()->getContents(), true);
+
+            $soldTickets = [];
+            foreach ($tickets['result'] as $ticket) {
+                if(($ticket['ticketStatusName'] === "Проданный") && ($ticket['tarifName'] !== "Пригласительное место")) {
+                    array_push($soldTickets, $ticket);
+                }
+            }
+
+            $soldSeats = [];
+            foreach ($soldTickets as $ticket) {
+                foreach ($seats['result'] as $seat) {
+                    if(($seat['sectorName'] === $ticket['sectorName']) && ($seat['seatNumber'] === (int)$ticket['seatNumber']) && ($seat['rowNumber'] === (int)$ticket['rowNumber'])) {
+                        array_push($soldSeats, $seat);
+                    } else {
+
+                    }
+                }
+            }
+
+            if (!empty($soldSeats)){
+                foreach ($soldSeats as $soldSeat) {
+                    $model = new Saver();
+                    $model->event_id = $event->id;
+                    $model->seat_id = 'seat-' . $soldSeat['svgSeatId'];
+                    $model->place_title = 'Sector: ' . $soldSeat['sectorName'] . ' Row: ' . $soldSeat['rowNumber'] . ' Seat: ' . $soldSeat['seatNumber'];
+                    $model->comment = 'Проданное место';
+                    $model->color = 'CCCCCC';
+                    if(Saver::find()->where(['event_id'=>$event->id,'seat_id'=>$model->seat_id])->one()){
+                        $a = 1;
+                    } else{
+                        $model->save();
+                    }
+                }
+            }
+        }
+
+
+    }
+}
